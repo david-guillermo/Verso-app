044cce046ce1329404c6395a65e2f28344d9b8fd
david-guillermo <davicraft.yt15@gmail.com>
2024-09-17 20:49:54 -0400

    02-verso

--- Verso-app/src/Components/Nav.jsx
+++ Verso-app/src/Components/Nav.jsx
@@ -1,26 +1,31 @@
+import { useState } from "react";
 import { Link } from "react-scroll";
+import { FaTimes } from "react-icons/fa";
+import { CiMenuFries } from "react-icons/ci";
 
 const  Nav = () => {
+    const [click, setClick] = useState(false);
+    const handleClick = () =>  setClick(!click);
     const content = <>
-    <div className="">
-        <ul>
-            <Link to="Home">
-                <li>Home</li>
+    <div className="lg:hidden block absolute top-16 w-full left-0 right-0 bg-zinc-900 transition">
+        <ul className="text-center text-xl p-20">
+            <Link spy={true} smooth={true} to="Home">
+                <li   className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Home</li>
             </Link>
-            <Link to="Acout">
-                <li>Acout</li>
+            <Link spy={true} smooth={true} to="Acout">
+                <li   className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Acout</li>
             </Link>
-            <Link to="About">
-                <li>About</li>
+            <Link spy={true} smooth={true} to="About">
+                <li   className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">About</li>
             </Link>
-            <Link to="Contact">
-                <li>Contact</li>
+            <Link spy={true} smooth={true} to="Contact">
+                <li   className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Contact</li>
             </Link>
-            <Link to="Service">
-                <li>Service</li>
+            <Link spy={true} smooth={true} to="Service">
+                <li   className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Service</li>
             </Link>
-            <Link to="Footer">
-                <li>Footer</li>
+            <Link spy={true} smooth={true} to="Footer">
+                <li   className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">Footer</li>
             </Link>
         </ul>
     </div>
@@ -29,32 +34,38 @@ const  Nav = () => {
         <nav>
             <div className="h-10vh flex justify-between z-50 text-white lg:py-5 px-20 py-4">
                 <div className="flex items-center flex-1">
-                    <span className="text-3xl font-bold">Logo</span>
+                    <img className="w-[70px]" src="./src/Verso-img/V-removebg-preview.png"></img>
                 </div>
                 <div className="lg:flex md:flex lg: flex-1 items center justify-end font-normal hidden">
                     <div className="flex-10">
                         <ul className="flex gap-8 mr-16 text-[18px]">
-                            <Link to="Home">
-                                <li>Home</li>
+                            <Link spy={true} smooth={true} to="Home">
+                                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Home</li>
                             </Link>
-                            <Link to="Acout">
-                                <li>Acout</li>
+                            <Link spy={true} smooth={true} to="Acout">
+                                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Acout</li>
                             </Link>
-                            <Link to="About">
-                                <li>About</li>
+                            <Link spy={true} smooth={true} to="About">
+                                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">About</li>
                             </Link>
-                            <Link to="Contact">
-                                <li>Contact</li>
+                            <Link spy={true} smooth={true} to="Contact">
+                                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Contact</li>
                             </Link>
-                            <Link to="Service">
-                                <li>Service</li>
+                            <Link spy={true} smooth={true} to="Service">
+                                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Service</li>
                             </Link>
-                            <Link to="Footer">
-                                <li>Footer</li>
+                            <Link spy={true} smooth={true} to="Footer">
+                                <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Footer</li>
                             </Link>
                         </ul>
                     </div>
                 </div>
+                <div>
+                    {click && content}
+                </div>
+                <button className="block sm:hidden transition" onClick={handleClick}>
+                    { click ? <FaTimes/> : <CiMenuFries/>}
+                </button>
             </div>
         </nav>
     );
